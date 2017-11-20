cd11b8624f658c9b76e4dc4a1f2d2f04b051d9a9
Ndanji Mukumbe <Ndanji.Mukumbe@investec.co.za>
2017-11-20 18:04:48 +0200

    added checks for straight flush and flush

--- Cards.py
+++ Cards.py
@@ -2,22 +2,84 @@ import sys
 import HandClass
 
 def CheckHand(suits, ranks):
-	#check for 5 of a kind
 	ranking = list(ranks)
 	uniqueRanks = []
-	numberOccurences = []
+	numberRankOccurences = []
+
+	handSuits = list(suits)
+	uniqueSuits = []
+	numberSuitOccurrences = []
 
 	for i in ranking:
 		if i not in uniqueRanks:
 			uniqueRanks.append(i)
 
 	for i in uniqueRanks:
-		numberOccurences.append(ranking.count(i))
+		numberRankOccurences.append(ranking.count(i))
 
-	if "Jk" in ranking and len(uniqueRanks)==2 and (4 in numberOccurences):
+	for i in handSuits:
+		if i not in uniqueSuits:
+			uniqueSuits.append(i)
+
+	for i in uniqueSuits:
+		numberSuitOccurrences.append(handSuits.count(i))
+
+
+	#check for 5 or 4 of a kind
+	if "Jk" in ranking and len(uniqueRanks)==2 and (4 in numberRankOccurences):
 		return "Five of a kind"
+	elif "JK" not in ranking and len(uniqueRanks)==2 and (4 in numberRankOccurences):
+		return "Four of a kind"
+
+	#check if straight flush
+	if len(uniqueSuits)==1 and IsSequential(ranking):
+		return "Straight flush"
+	elif len(uniqueSuits)==1 and not IsSequential(ranking):
+		return "Flush"
 		
+def IsSequential(ranks):
+	isSequen = True
+	#special cases
+	if "K" in ranks and "A" in ranks:
+		if "Q" not in ranks or "J" not in ranks or "10" not in ranks:
+			isSequen = False
+	elif "K" in ranks and "A" not in ranks:
+		if "Q" not in ranks or "J" not in ranks or "10" not in ranks or "9" not in ranks:
+			isSequen = False
+	elif "Q" in ranks:
+		if "J" not in ranks or "10" not in ranks or "9" not in ranks or "8" not in ranks:
+			isSequen = False
+	elif "J" in ranks:
+		if "7" not in ranks or "10" not in ranks or "9" not in ranks or "8" not in ranks:
+			isSequen = False
+	elif "A" in ranks:
+		if "2" not in ranks or "3" not in ranks or "4" not in ranks or "5" not in ranks:
+			isSequen = False
+	else:
+		counter = 0
+		if not IsNumber(ranks[0]):
+			counter = int(len(ranks))
+			for i in range(len(ranks)-1,0,-1):
+				counter = counter + 1
+				if ranks[i] != counter:
+					isSequen = False
+		else:
+			counter = int(ranks[len(ranks)-1])
 			
+			for i in range(len(ranks)-1,-1,-1):
+				
+				if int(ranks[i]) != counter:
+					isSequen = False
+				counter = counter + 1
+
+	return isSequen
+
+def IsNumber(rank):
+	try:
+		int(rank)
+		return True
+	except ValueError:
+		return False
 
 
 def main():
@@ -25,6 +87,8 @@ def main():
   cards = userInput.split(',')
 
   hand = HandClass.Hand(cards)
+
+  print(hand.Rank)
 
   result = CheckHand(hand.Suits, hand.Rank)
 
--- CardsTests.py
+++ CardsTests.py
@@ -11,5 +11,45 @@ class HandClassTestCase(unittest.TestCase):
 		result = Cards.CheckHand(hand.Suits, hand.Rank)
 		self.assertEqual(result,"Five of a kind")
 
+	def test_cards_four_of_a_kind_successful(self):
+		cards = {"AS","AC","AH","AD","KH"}
+
+		hand = HandClass.Hand(cards)
+
+		result = Cards.CheckHand(hand.Suits, hand.Rank)
+		self.assertEqual(result,"Four of a kind")
+
+	def test_cards_special_straight_flush_successful(self):
+		cards = {"AH","KH","QH","JH","10H"}
+
+		hand = HandClass.Hand(cards)
+
+		result = Cards.CheckHand(hand.Suits, hand.Rank)
+		self.assertEqual(result,"Straight flush")
+
+	def test_cards_straight_flush_successful(self):
+		cards = {"8D","7D","6D","5D","4D"}
+
+		hand = HandClass.Hand(cards)
+
+		result = Cards.CheckHand(hand.Suits, hand.Rank)
+		self.assertEqual(result,"Straight flush")
+
+	def test_cards_special_flush_successful(self):
+		cards = {"AH","3H","QH","7H","10H"}
+
+		hand = HandClass.Hand(cards)
+
+		result = Cards.CheckHand(hand.Suits, hand.Rank)
+		self.assertEqual(result,"Flush")
+
+	def test_cards_Flush_successful(self):
+		cards = {"8D","2D","6D","9D","4D"}
+
+		hand = HandClass.Hand(cards)
+
+		result = Cards.CheckHand(hand.Suits, hand.Rank)
+		self.assertEqual(result,"Flush")
+
 if __name__ == '__main__':
 	unittest.main()
